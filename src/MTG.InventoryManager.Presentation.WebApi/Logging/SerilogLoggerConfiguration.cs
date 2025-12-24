e36e819a00fbdc6567e5772cace4d6a2fa2d2fa0
Marino Bjelopera <marino.bjelopera26@gmail.com>
2025-12-24 22:22:28 +0100

    Setup Serilog as the logging provider

---
+++ src/MTG.InventoryManager.Presentation.WebApi/Logging/SerilogLoggerConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace MTG.InventoryManager.Presentation.WebApi.Logging;
+
+internal static class SerilogLoggerConfiguration
+{
+    public static LoggerConfiguration GetLoggerConfiguration(IHostEnvironment environment)
+    {
+        var minimumLevel = environment.IsDevelopment()
+            ? LogEventLevel.Debug 
+            : LogEventLevel.Information;
+
+        var loggerConfig = new LoggerConfiguration()
+            .MinimumLevel.Is(minimumLevel)
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithMachineName()
+            .Enrich.WithProcessId()
+            .Enrich.WithThreadId()
+            .Enrich.WithThreadName();
+
+        loggerConfig.WriteTo
+            .Console(
+                minimumLevel,
+                outputTemplate: "[{Timestamp:HH:mm:ss.zzz} {Level:u3}] {CorrelationId} {SourceContext} {Message:lj}{NewLine}{Exception}",
+                theme: AnsiConsoleTheme.Code);
+        
+        return loggerConfig;
+    }
+}
