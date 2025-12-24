af09599cfa2b8796f4efc95276432c602907eb5a
marinobjelopera26 <64930112+marinobjelopera26@users.noreply.github.com>
2025-12-24 22:23:39 +0100

    Setup Serilog as the logging provider (#4)

--- src/MTG.InventoryManager.Presentation.WebApi/Program.cs
+++ src/MTG.InventoryManager.Presentation.WebApi/Program.cs
@@ -1,10 +1,18 @@
+using MTG.InventoryManager.Presentation.WebApi.Logging;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+Log.Logger = SerilogLoggerConfiguration.GetLoggerConfiguration(builder.Environment).CreateLogger();
+builder.Host.UseSerilog(Log.Logger, dispose: true);
+
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,4 +22,16 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.Run();
+try
+{
+    Log.Information("Starting web host ({Environment})...)", app.Environment.EnvironmentName);
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
