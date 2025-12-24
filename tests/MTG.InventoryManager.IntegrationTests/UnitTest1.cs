a0cdf1d033c60d43956f056822dcba1ba2fd6597
marinobjelopera26 <64930112+marinobjelopera26@users.noreply.github.com>
2025-12-24 22:05:17 +0100

    Add nunit test projects (#3)

---
+++ tests/MTG.InventoryManager.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace MTG.InventoryManager.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
