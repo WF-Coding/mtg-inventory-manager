e7682ccbacc51046d01835e034becba4aca34fee
Marino Bjelopera <marino.bjelopera26@gmail.com>
2025-12-24 22:01:56 +0100

    add nunit test projects

---
+++ tests/MTG.InventoryManager.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace MTG.InventoryManager.FunctionalTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
